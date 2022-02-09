1678bf4c913eb4df2642af8b7676461932b777a5
Jacob Bremiller <48461435+jacob1421@users.noreply.github.com>
2022-02-08 23:05:42 -0600

    Updates

---
+++ dags/scripts/config.py
@@ -0,0 +1,9 @@
+TWITTER_USER_ID = 3243246400
+BUCKET_NAME = "rust-cheaters"
+BUCKET_REGION = "us-east-1"
+LANDING_RAW = "landing-zone/raw/"
+LANDING_STAGED = "landing-zone/staged/"
+AWS_CONN_ID = "s3"
+POSTGRES_CONN = "aws_postgres"
+DATABASE = "data_warehouse"
+SCHEMA = "rust_data_warehouse"
